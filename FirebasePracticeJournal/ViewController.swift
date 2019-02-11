e2640285d1892de78b8e82ecb89a41771fbd530b
Billsy22 <35415537+Billsy22@users.noreply.github.com>
2019-02-11 11:39:23 -0700

    first commit

---
+++ FirebasePracticeJournal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebasePracticeJournal
+//
+//  Created by Taylor Bills on 2/11/19.
+//  Copyright © 2019 Taylor Bills. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
